81e54f85735339ee85a8ad7d6476a5c4fc91ed3b
unknown <macherif@luxcarta.com>
2023-03-15 14:36:20 +0100

    minor: fixing no data value and direction of search between left and right images

--- app/include/fusion_ops/epipolar_disparities_combination.h
+++ app/include/fusion_ops/epipolar_disparities_combination.h
@@ -25,21 +25,21 @@ namespace LxGeo
 				add_raster_output_dataset(out_right_disparity_path, { "in_rd" }, ExtentsCombinationStrategy::ext_intersection, "out_rd");
 				add_raster_output_dataset(left_certainty_map_path, { "in_ld" }, ExtentsCombinationStrategy::ext_intersection, "out_lcm");
 
-				init_cpd(OperationDiveStrategy::zoom, 1000.0, 200.0);
+				init_cpd(OperationDiveStrategy::zoom, 3000.0, 500.0);
 
 			};
 
 			ViewPair op (ViewPair& in_view_pair) override {
 
-				double TEMP_NO_DATA = 0.0;
+				int16_t TEMP_NO_DATA = -32768;
 
 				ViewPair out_view;
 				const cv::Mat& in_ld = in_view_pair.raster_views["in_ld"].image;
 				const cv::Mat& in_rd = in_view_pair.raster_views["in_rd"].image;
 
 				// Init output maps
-				out_view.raster_views["out_ld"] = GeoImage<cv::Mat>(in_view_pair.raster_views["in_ld"]);
-				out_view.raster_views["out_rd"] = GeoImage<cv::Mat>(in_view_pair.raster_views["in_rd"]);
+				out_view.raster_views["out_ld"] = GeoImage<cv::Mat>((cv::Mat)(cv::Mat::ones(in_ld.size(), CV_16SC1)* TEMP_NO_DATA), in_view_pair.raster_views["in_ld"].geotransform); //GeoImage<cv::Mat>(in_view_pair.raster_views["in_ld"]);
+				out_view.raster_views["out_rd"] = GeoImage<cv::Mat>((cv::Mat)(cv::Mat::ones(in_rd.size(), CV_16SC1) * TEMP_NO_DATA), in_view_pair.raster_views["in_rd"].geotransform);//GeoImage<cv::Mat>(in_view_pair.raster_views["in_rd"]);
 				out_view.raster_views["out_lcm"] = GeoImage<cv::Mat>((cv::Mat)cv::Mat::zeros(in_ld.size(), CV_16SC1), in_view_pair.raster_views["in_ld"].geotransform);
 
 				double l_px, r_px, diff;
@@ -48,20 +48,20 @@ namespace LxGeo
 					for (int y = 0; y < in_ld.rows; y++) {
 						// fill right image
 						l_px = in_ld.at<int16_t>(y, x);
-						xpos_in_other_image = x + l_px/10;
+						xpos_in_other_image = x - l_px/10;
 						
 						if (xpos_in_other_image >= 0 && xpos_in_other_image < in_ld.cols && l_px!=TEMP_NO_DATA) {
-							if (in_rd.at<int16_t>(y, xpos_in_other_image) == TEMP_NO_DATA)
+							if (out_view.raster_views["out_rd"].image.at<int16_t>(y, xpos_in_other_image) == TEMP_NO_DATA)
 								out_view.raster_views["out_rd"].image.at<int16_t>(y, xpos_in_other_image) = -l_px;
 							else
 								out_view.raster_views["out_lcm"].image.at<int16_t>(y,x) = abs(l_px + out_view.raster_views["out_rd"].image.at<int16_t>(y, xpos_in_other_image));
 						}
 
 						r_px = in_rd.at<int16_t>(y,x);
-						xpos_in_other_image = x + r_px/10;
+						xpos_in_other_image = x - r_px/10;
 						if (xpos_in_other_image >= 0 && xpos_in_other_image < in_ld.cols && r_px!=TEMP_NO_DATA) {
-							if (in_rd.at<int16_t>(y, xpos_in_other_image) == TEMP_NO_DATA)
-								out_view.raster_views["out_ld"].image.at<int16_t>(y, xpos_in_other_image) = -l_px;
+							if (out_view.raster_views["out_ld"].image.at<int16_t>(y, xpos_in_other_image) == TEMP_NO_DATA)
+								out_view.raster_views["out_ld"].image.at<int16_t>(y, xpos_in_other_image) = -r_px;
 						}
 
 					}
