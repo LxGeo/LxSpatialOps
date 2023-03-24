4e8ea0e7cc38eb761c70227096f6035eeb7ad606
unknown <macherif@luxcarta.com>
2023-03-24 17:12:49 +0100

    recalling to opencv forEach to parrellize epipolar dipsarity fusion

--- app/include/fusion_ops/epipolar_disparities_combination.h
+++ app/include/fusion_ops/epipolar_disparities_combination.h
@@ -42,7 +42,7 @@ namespace LxGeo
 				out_view.raster_views["out_rd"] = GeoImage<cv::Mat>((cv::Mat)(cv::Mat::ones(in_rd.size(), CV_16SC1) * TEMP_NO_DATA), in_view_pair.raster_views["in_rd"].geotransform);//GeoImage<cv::Mat>(in_view_pair.raster_views["in_rd"]);
 				out_view.raster_views["out_lcm"] = GeoImage<cv::Mat>((cv::Mat)cv::Mat::zeros(in_ld.size(), CV_16SC1), in_view_pair.raster_views["in_ld"].geotransform);
 
-				double l_px, r_px, diff;
+				/*double l_px, r_px, diff;
 				int xpos_in_other_image;
 				for (int x = 0; x < in_ld.cols; x++) {
 					for (int y = 0; y < in_ld.rows; y++) {
@@ -65,7 +65,32 @@ namespace LxGeo
 						}
 
 					}
-				}
+				}*/
+
+				// Better only for release mode
+				in_ld.forEach<int16_t>([&in_rd, &out_view, &TEMP_NO_DATA](int16_t& l_px, const int position[])->void {
+					cv::Mat& out_rigth_image = out_view.raster_views["out_rd"].image;
+					cv::Mat& out_left_image = out_view.raster_views["out_ld"].image;
+					cv::Mat& out_left_cm = out_view.raster_views["out_lcm"].image;
+
+					int xpos_in_other_image = position[1] - l_px / 10;
+					if (xpos_in_other_image >= 0 && xpos_in_other_image < out_left_image.cols && l_px != TEMP_NO_DATA) {
+						if (out_rigth_image.at<int16_t>(position[0], xpos_in_other_image) == TEMP_NO_DATA)
+							out_rigth_image.at<int16_t>(position[0], xpos_in_other_image) = -l_px;
+						else
+							out_left_cm.at<int16_t>(position[0], position[1]) = abs(l_px + out_rigth_image.at<int16_t>(position[0], xpos_in_other_image));
+					}
+
+					int16_t r_px = in_rd.at<int16_t>(position[0], position[1]);
+					xpos_in_other_image = position[1] - r_px / 10;
+					if (xpos_in_other_image >= 0 && xpos_in_other_image < out_left_image.cols && r_px != TEMP_NO_DATA) {
+						if (out_left_image.at<int16_t>(position[0], xpos_in_other_image) == TEMP_NO_DATA)
+							out_left_image.at<int16_t>(position[0], xpos_in_other_image) = -r_px;
+					}
+
+
+					});
+
 
 				return out_view;
 
