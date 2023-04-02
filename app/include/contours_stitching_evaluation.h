b216ccd10f43e71c0f15c97092e884a674525c00
sherif-med <cherif.abedrazek@gmail.com>
2023-04-03 01:33:57 +0200

    add write mode based on file existance && adding valid_geometry index init && using numpcpp stats

--- app/include/contours_stitching_evaluation.h
+++ app/include/contours_stitching_evaluation.h
@@ -3,6 +3,7 @@
 #include "base_operation.h"
 #include "spatial_datasets/patchified_dataset.h"
 #include "stitching/vector_on_raster_stitcher.h"
+#include "numcpp/stats.h"
 
 namespace LxGeo
 {
@@ -20,7 +21,9 @@ namespace LxGeo
 
 				add_raster_input_dataset(proximity_map_path, "proximity");
 				add_vector_input_dataset(polygons_map_path, "polys");
-				add_vector_output_dataset(out_polygons_map_path, { "polys" }, ext_intersection, WriteMode::create, "out_polys");
+				add_vector_output_dataset(out_polygons_map_path, { "polys" }, ext_intersection,
+					(out_polygons_map_path.compare(polygons_map_path)!=0)? WriteMode::create: WriteMode::update,
+					"out_polys");
 				init_cpd(OperationDiveStrategy::zoom, 1000.0, 200.0);
 
 			}
@@ -30,13 +33,15 @@ namespace LxGeo
 				ViewPair out_view;
 
 				out_view.vector_views["out_polys"] = boost::get<GeoVector<Boost_Polygon_2>>(in_view_pair.vector_views["polys"]);
+				out_view.valid_geometries_indices["out_polys"].idx = 0;
 				GeoVector<Boost_Polygon_2>& gvec = boost::get<GeoVector<Boost_Polygon_2>>(out_view.vector_views["out_polys"]);
 
 				RasterPixelsStitcher rps = RasterPixelsStitcher(in_view_pair.raster_views["proximity"]);
 
 				for (auto& gwa : gvec.geometries_container) {
 					auto stitched_pixels = rps.readPolygonPixels<float>(gwa.get_definition(), RasterPixelsStitcherStartegy::contours);
-					double stitched_sum = std::accumulate(stitched_pixels.begin(), stitched_pixels.end(), 0.0);
+					auto stats = numcpp::DetailedStats<float>(stitched_pixels, FLT_MAX, 0.0);
+					double stitched_sum = stats.sum();
 					double confidence = stitched_sum / bg::area(gwa.get_definition().outer()); 
 					gwa.set_double_attribute("confidence", confidence);
 				}
